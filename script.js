ea0d7567f81facf7d278cf2da57f7f0627352fc1
João Vitor Madureira <76231431+joaomadureira24@users.noreply.github.com>
2021-10-14 20:36:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let pessoa = {
+    nome: "Jonathan",
+    idade: 32,
+    cidade: "Taguaraí"
+}
+
+let pessoa_um = {
+    nome: "Miguélito",
+    idade: 7,
+    cidade: "São João Novo"
+}
+
+let pessoa_dois = {
+    nome: "Thiago",
+    idade: 12,
+    cidade: "São Roque"
+}
+
+let array_people =[pessoa, pessoa_um, pessoa_dois];
+let [Jonathan, Miguélito, Thiago] = array_people;
+console.log(Jonathan)
+console.log(Miguélito);
